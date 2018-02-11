93ae968b0cb58b554ecc0b557a20ffe9b651b1c6
Ethan Mertz <ethanmertz@cs.uchicago.edu>
2018-02-10 18:18:06 -0600

    added fitter

---
+++ fitter.py
@@ -0,0 +1,15 @@
+# Algorithm to fit the constelations to the mcdonalds locations
+# Ethan Mertz 2018
+
+import sqlite
+import math
+import csv
+import constellation
+
+
+def fit_points(point1, point2, constellation):
+
+
+
+
+
